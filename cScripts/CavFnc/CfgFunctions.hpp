b3bacaf6f39a1ecb8bcbf737eb33777fad2ec61f
ColdEvul <andreas.brostrom.ce@gmail.com>
2016-12-17 23:03:20 +0100

    Added addHeal to CfgFunctions

--- cScripts/CavFnc/CfgFunctions.hpp
+++ cScripts/CavFnc/CfgFunctions.hpp
@@ -39,6 +39,7 @@ class cScripts {
     class systems {
         file = "cScripts\cavFnc\functions\systems";
         class addReGear {};
+        class addHeal {};
         class addArsenal {};
         class addAceCategory {};
     };
