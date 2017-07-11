8aeb2860de5bc8af23ba983b2b293027b2c229a9
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2017-07-11 14:02:05 +0200

    Removed leftover code

--- cScripts/Loadouts/CfgLoadouts_Ranger.hpp
+++ cScripts/Loadouts/CfgLoadouts_Ranger.hpp
@@ -61,19 +61,3 @@ class CAV_Spotter : CAV_Sniper {
     gps[] = {"ItemAndroid"};
     watch[] = {"ACE_Altimeter"};
 };
-
-// Exported from Arsenal by CPL.Brostrom.A
-uniform = "rhs_uniform_cu_ocp_1stcav";
-backpack = "tf_rt1523g";
-weapons[] = {"rhs_weap_m4a1_carryhandle","ACE_Vector","Leupold_Mk4","Throw","Put"};
-magazines[] = {"rhs_mag_m67","rhs_mag_m67","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer","rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer","rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer","rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer","rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_m67","rhs_mag_m67","rhs_mag_an_m8hc"};
-items[] = {"ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_EarPlugs","ACE_EarPlugs","ACE_ATragMX","ACE_tourniquet","ACE_morphine","ACE_morphine","ACE_Flashlight_MX991","ACE_Kestrel4500","ACE_microDAGR","ACE_MapTools","ACE_RangeCard","ACE_personalAidKit","Leupold_Mk4"};
-linkedItems[] = {"rhsusf_spcs_ocp","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ACE_Altimeter","tf_anprc152_5","ItemAndroid"};
-
-// Exported from Arsenal by CPL.Brostrom.A
-uniform = "rhs_uniform_cu_ocp_1stcav";
-backpack = "B_AssaultPack_cbr";
-weapons[] = {"rhs_weap_sr25_d","ACE_Vector","Leupold_Mk4","Throw","Put"};
-magazines[] = {"rhs_mag_m67","rhs_mag_m67","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhs_mag_m67","rhs_mag_m67","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhs_mag_an_m8hc","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag","rhsusf_20Rnd_762x51_m118_special_Mag"};
-items[] = {"ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_quikclot","ACE_EarPlugs","ACE_EarPlugs","ACE_ATragMX","ACE_tourniquet","ACE_morphine","ACE_morphine","ACE_Flashlight_MX991","ACE_Kestrel4500","ACE_microDAGR","ACE_MapTools","ACE_RangeCard","ACE_personalAidKit","Leupold_Mk4"};
-linkedItems[] = {"rhsusf_spcs_ocp","rhsusf_ach_helmet_ocp","","ItemMap","ItemCompass","ACE_Altimeter","tf_anprc152_5","ItemAndroid"};
