558cee3aa434bedfa97d760a36a2cb54ae3aa1be
ColdEvul <andreas.brostrom.ce@gmail.com>
2017-11-15 16:24:33 +0100

    removed delta

--- cScripts/Loadouts/CfgLoadouts_Common.hpp
+++ cScripts/Loadouts/CfgLoadouts_Common.hpp
@@ -128,38 +128,6 @@ class CAV_Charlie_Base : CommonBlufor {
         [(_this select 0)] call ace_hearing_fnc_putInEarplugs";
 };
 
-class CAV_Delta_Base : CommonBlufor {
-    backpack[] = {""};
-    goggles[] = {""};
-    headgear[] = {""};
-    //uniform[] = {"rhs_uniform_cu_ocp_1stcav"};
-    vest[] = {""};
-
-    primary[] = {""};
-    secondary[] = {""};
-    launcher[] = {""};
-    
-    binoculars[] = {""};
-
-    magazines[] = {""};
-    items[] = {""};
-
-    compass[] = {""};
-    gps[] = {""};
-    map[] = {""};
-    nvgs[] = {""};
-    watch[] = {""};
-    
-    insignia[] = {""};
-    preLoadout = " \
-        (_this select 0) setVariable [""CAV_isDeltaUnit"", true]; \
-        (_this select 0) setVariable [""ACE_medical_medicClass"", 0, true]; \
-        (_this select 0) setVariable [""ACE_isEngineer"", false];";
-    postLoadout = " \
-        [(_this select 0), currentWeapon (_this select 0), currentMuzzle (_this select 0)] call ace_safemode_fnc_lockSafety; \
-        [(_this select 0)] call ace_hearing_fnc_putInEarplugs";
-};
-
 class CAV_Ranger_Base : CommonBlufor {
     backpack[] = {""};
     goggles[] = {""};
