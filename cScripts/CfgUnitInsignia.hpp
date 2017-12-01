916106c238a9d07cebe295d027f4746e1586492e
ColdEvul <andreas.brostrom.ce@gmail.com>
2017-12-01 21:31:31 +0100

    Fixed indent

--- cScripts/CfgUnitInsignia.hpp
+++ cScripts/CfgUnitInsignia.hpp
@@ -6,14 +6,14 @@ class CfgUnitInsignia {
     MACRO_UNITINSIGNIA(C1A_17_Insignia,C-1-A-17.paa);
     MACRO_UNITINSIGNIA(C2A_17_Insignia,C-2-A-17.paa);
 
-	MACRO_UNITINSIGNIA(11C_17_Insignia,1-1-C-17.paa);
+    MACRO_UNITINSIGNIA(11C_17_Insignia,1-1-C-17.paa);
     MACRO_UNITINSIGNIA(21C_17_Insignia,2-1-C-17.paa);
     //MACRO_UNITINSIGNIA(31C_17_Insignia,3-1-C-17.paa);
     //MACRO_UNITINSIGNIA(41C_17_Insignia,4-1-C-17.paa);
 
     // Platoon Insignias
     MACRO_UNITINSIGNIA(2A_17_Insignia,2-A-17.paa);
-	MACRO_UNITINSIGNIA(2C_17_Insignia,2-C-17.paa);
+    MACRO_UNITINSIGNIA(2C_17_Insignia,2-C-17.paa);
 
     // Other Insignias
     MACRO_UNITINSIGNIA(7_Insignia,7.paa);
