0d18733b82eb3a86b9548b67c4fd5955141da6a5
ColdEvul <andreas.brostrom.ce@gmail.com>
2018-01-22 22:57:02 +0100

    Updated parameters

--- cScripts/Loadouts/script/CfgPoppy.hpp
+++ cScripts/Loadouts/script/CfgPoppy.hpp
@@ -1,14 +1,14 @@
 class CfgPoppy {
     forceShowInfos = 0;
     forceShowWarnings = 0;
-    forceShowErrors = 1;
-    showLoadoutInBriefing = 1;
+    forceShowErrors = 0;
+    showLoadoutInBriefing = 0;
     enableAILoadoutsSP = 1;
     //Not implemented for 1.0.0. May be implemented with CBA 2.3.0.
     //enableAILoadoutsMP = 0;
 
     class ACRE {
-        distributeGroupLeaderRadios = 0;
+        distributeGroupLeaderRadios = 1;
         groupLeaderRadio = "ACRE_PRC152";
         channelNames[] = {"CMDNET", "CAS"};
     };
