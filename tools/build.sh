0f80fec55c3a7a5607b92bcd7c8f26f6ba6f64d0
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-02-26 10:48:08 +0100

    Fixed

--- tools/build.sh
+++ tools/build.sh
@@ -4,6 +4,6 @@ set -e
 
 sed -i "s/#define VERSION.*/#define VERSION ${TRAVIS_TAG}/" cScripts/script_component.hpp
 
-python3 tools/build.py -b release -d -y
+python3 tools/build.py -b release -d -y --deploy
 
-python3 tools/build.py -b release -p -d -y
+python3 tools/build.py -b release -p -d -y --deploy
