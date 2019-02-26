eb9f55f588b9d9630203bcc2a6676e4b8aa7a961
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-02-26 11:33:44 +0100

    fixed formating

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sed -i "s/#define VERSION.*/#define VERSION ${TRAVIS_TAG}/" cScripts/script_component.hpp
+sed -i "s/#define VERSION.*/#define VERSION \"${TRAVIS_TAG}\"/" cScripts/script_component.hpp
 
 python3 tools/build.py -b release -d -y --deploy
 
