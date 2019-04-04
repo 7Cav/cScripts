62cc0a0a793d0eda9c4d061ee793e6f74b81ceb9
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-04-04 16:45:12 +0200

    Converted Loadouts to to utalize new classnames

--- cScripts/CfgLoadouts.hpp
+++ cScripts/CfgLoadouts.hpp
@@ -11,14 +11,11 @@ class CfgLoadouts {
     #include "Loadouts\CfgLoadouts_Charlie.hpp"
     #include "Loadouts\CfgLoadouts_CharlieClass.hpp"
 
-    #include "Loadouts\CfgLoadouts_Medical.hpp"
-    #include "Loadouts\CfgLoadouts_MedicalClass.hpp"
+    #include "Loadouts\CfgLoadouts_Ranger.hpp"          // Not compleeted
+    #include "Loadouts\CfgLoadouts_RangerClass.hpp"     // Not compleeted
 
-    #include "Loadouts\CfgLoadouts_Ranger.hpp"
-    #include "Loadouts\CfgLoadouts_RangerClass.hpp"
-
-    #include "Loadouts\CfgLoadouts_Training.hpp"
-    #include "Loadouts\CfgLoadouts_TrainingClass.hpp"
+    #include "Loadouts\CfgLoadouts_Training.hpp"        // Not compleeted
+    #include "Loadouts\CfgLoadouts_TrainingClass.hpp"   // Not compleeted
 
     #include "Loadouts\CfgLoadouts_S3.hpp"
     #include "Loadouts\CfgLoadouts_S5.hpp"
