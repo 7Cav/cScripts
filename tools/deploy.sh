13e2955e68589e53e12e721406a4bbf7ed45cb1f
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-06-19 17:32:56 +0200

    Change path

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -12,6 +12,6 @@ sed -i "s/DEVBUILD/${TRAVIS_TAG}/g" Compositions/*/header.sqe
 
 PREV_TAG=git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`
 
-git archive -o cScripts_PATCH_v${PREV_TAG}_to_v${TRAVIS_TAG}.zip HEAD $(git diff --name-only ${TRAVIS_TAG}^ ${PREV_TAG})
+git archive -o "release/cScripts_PATCH_v${PREV_TAG}_to_v${TRAVIS_TAG}.zip" HEAD $(git diff --name-only ${TRAVIS_TAG}^ ${PREV_TAG})
 
 zip release/Compositions-${TRAVIS_TAG}.zip -r Compositions
