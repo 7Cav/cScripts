d8286c84ab04904b353b07b14ac4ba93a76dc3a6
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-06-19 17:29:00 +0200

    patch release should now be built

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -10,4 +10,8 @@ python3 tools/build.py -b release -p -d -y --deploy
 
 sed -i "s/DEVBUILD/${TRAVIS_TAG}/g" Compositions/*/header.sqe
 
+PREV_TAG=git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`
+
+git archive -o cScripts_PATCH_v${PREV_TAG}_to_v${TRAVIS_TAG}.zip HEAD $(git diff --name-only ${TRAVIS_TAG}^ ${PREV_TAG})
+
 zip release/Compositions-${TRAVIS_TAG}.zip -r Compositions
