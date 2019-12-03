e9a4b2273f26f4b9c1bb31064fdf16bbcf78abf0
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-12-03 18:06:44 +0100

    dependency installation

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-sed -i "s/#define VERSION.*/#define VERSION \"${TRAVIS_TAG}\"/" cScripts/script_component.hpp
+sed -i "s/#define VERSION.*/#define VERSION \"${github.ref}\"/" cScripts/script_component.hpp
 
 python3 tools/build.py -b release -d -y --deploy
 
 python3 tools/build.py -b release -p -d -y --deploy
 
-sed -i "s/DEVBUILD/${TRAVIS_TAG}/g" Compositions/*/header.sqe
+sed -i "s/DEVBUILD/${github.ref}/g" Compositions/*/header.sqe
 
 # Make a patch release
 PREV_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
-zip release/cScripts_PATCH_v${PREV_TAG}_to_v${TRAVIS_TAG}.zip `git diff --name-only tags/${TRAVIS_TAG}^ tags/${PREV_TAG}` -x=*Compositions* -x=*tools* -x=*resourses* -x=*.github* -x=*.travis.yml* -x=*.gitignore* -x=*.gitattributes* -x=*.editorconfig*
+zip release/cScripts_PATCH_v${PREV_TAG}_to_v${github.ref}.zip `git diff --name-only tags/${github.ref}^ tags/${PREV_TAG}` -x=*Compositions* -x=*tools* -x=*resourses* -x=*.github* -x=*.travis.yml* -x=*.gitignore* -x=*.gitattributes* -x=*.editorconfig*
 
 # Pack Compositions
-zip release/Compositions-${TRAVIS_TAG}.zip -r Compositions
+zip release/Compositions-${github.ref}.zip -r Compositions
