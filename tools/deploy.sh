4f2be714eaf12295bbb0fa75d0f48758f765b8e5
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-12-03 18:24:30 +0100

    removed patch

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -13,8 +13,8 @@ python3 tools/build.py -b release -p -d -y --deploy
 sed -i "s/DEVBUILD/${Verions_NR}/g" Compositions/*/header.sqe
 
 # Make a patch release
-PREV_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
-zip release/cScripts_PATCH_v${PREV_TAG}_to_v${Verions_NR}.zip `git diff --name-only tags/${Verions_NR} -- tags/${PREV_TAG}` -x=*Compositions* -x=*tools* -x=*resourses* -x=*.github* -x=*.travis.yml* -x=*.gitignore* -x=*.gitattributes* -x=*.editorconfig*
+#PREV_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
+#zip release/cScripts_PATCH_v${PREV_TAG}_to_v${Verions_NR}.zip `git diff --name-only tags/${Verions_NR} -- tags/${PREV_TAG}` -x=*Compositions* -x=*tools* -x=*resourses* -x=*.github* -x=*.travis.yml* -x=*.gitignore* -x=*.gitattributes* -x=*.editorconfig*
 
 # Pack Compositions
 zip release/Compositions-${Verions_NR}.zip -r Compositions
