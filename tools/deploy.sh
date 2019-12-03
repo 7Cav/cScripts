44b2d8680214c8e10aeadd04d5c9af4e36e194e5
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-12-03 18:09:49 +0100

    script param

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-sed -i "s/#define VERSION.*/#define VERSION \"${github.ref}\"/" cScripts/script_component.hpp
+Verions_NR = $*
+
+sed -i "s/#define VERSION.*/#define VERSION \"${Verions_NR}\"/" cScripts/script_component.hpp
 
 python3 tools/build.py -b release -d -y --deploy
 
 python3 tools/build.py -b release -p -d -y --deploy
 
-sed -i "s/DEVBUILD/${github.ref}/g" Compositions/*/header.sqe
+sed -i "s/DEVBUILD/${Verions_NR}/g" Compositions/*/header.sqe
 
 # Make a patch release
 PREV_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
-zip release/cScripts_PATCH_v${PREV_TAG}_to_v${github.ref}.zip `git diff --name-only tags/${github.ref}^ tags/${PREV_TAG}` -x=*Compositions* -x=*tools* -x=*resourses* -x=*.github* -x=*.travis.yml* -x=*.gitignore* -x=*.gitattributes* -x=*.editorconfig*
+zip release/cScripts_PATCH_v${PREV_TAG}_to_v${Verions_NR}.zip `git diff --name-only tags/${Verions_NR}^ tags/${PREV_TAG}` -x=*Compositions* -x=*tools* -x=*resourses* -x=*.github* -x=*.travis.yml* -x=*.gitignore* -x=*.gitattributes* -x=*.editorconfig*
 
 # Pack Compositions
-zip release/Compositions-${github.ref}.zip -r Compositions
+zip release/Compositions-${Verions_NR}.zip -r Compositions
