dad4f4ddc73dc2a9d855ba7c83489e4b08ebbe65
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-12-03 19:02:18 +0100

    allow fail on patch

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -12,9 +12,13 @@ python3 tools/build.py -b release -p -d -y --deploy
 
 sed -i "s/DEVBUILD/${VERSION_TAG}/g" Compositions/*/header.sqe
 
+
 # Make a patch release
+unset -e # allow fail
 PREV_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
 zip release/cScripts_PATCH_v${PREV_TAG}_to_v${VERSION_TAG}.zip `git diff --name-only tags/${VERSION_TAG}^ tags/${PREV_TAG}` -x=*Compositions* -x=*tools* -x=*resourses* -x=*.github* -x=*.travis.yml* -x=*.gitignore* -x=*.gitattributes* -x=*.editorconfig*
+
+set -e
 
 # Pack Compositions
 zip release/Compositions-${VERSION_TAG}.zip -r Compositions
