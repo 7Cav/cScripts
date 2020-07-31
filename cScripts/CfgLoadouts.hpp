806744750ab70fc72b09e59d44731b75d2dde2b3
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-07-31 22:04:55 +0200

    Removed all loadouts for restrujcture reasons

--- cScripts/CfgLoadouts.hpp
+++ cScripts/CfgLoadouts.hpp
@@ -1,24 +1,5 @@
 class CfgLoadouts {
-    #include "script_component.hpp"
     #include "Loadouts\CfgLoadouts_Common.hpp"
 
-    #include "Loadouts\CfgLoadouts_Alpha.hpp"
-    #include "Loadouts\CfgLoadouts_Bravo.hpp"
-    #include "Loadouts\CfgLoadouts_Bravo_Atlas.hpp"
-    #include "Loadouts\CfgLoadouts_Bravo_Scouts.hpp"
-
-    #include "Loadouts\CfgLoadouts_Charlie.hpp"
-
-    #include "Loadouts\CfgLoadouts_Training.hpp"        // Not compleeted
-
-    #include "Loadouts\CfgLoadouts_S3.hpp"
-    #include "Loadouts\CfgLoadouts_S5.hpp"
-
-    
-    #include "Loadouts\CfgLoadouts_AlphaClass.hpp"
-    #include "Loadouts\CfgLoadouts_BravoClass.hpp"
-    #include "Loadouts\CfgLoadouts_CharlieClass.hpp"
-    #include "Loadouts\CfgLoadouts_TrainingClass.hpp"   // Not compleeted
-    
     #include "Loadouts\CfgLoadouts_UserCustom.hpp"
 };
