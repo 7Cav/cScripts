ad6d2372c0ee0fffbafa89a835ed69c89dfb7a46
Nik Poliakov <niki.liber@gmail.com>
2020-10-15 17:23:15 +0300

    Tabs

--- cScripts/CavFnc/functions/garage/garage_gui.hpp
+++ cScripts/CavFnc/functions/garage/garage_gui.hpp
@@ -93,20 +93,20 @@
 #define TR_AUTOCOLLAPSE   2
 
 // Default grid
-#define GUI_GRID_WAbs			((safezoneW / safezoneH) min 1.2)
-#define GUI_GRID_HAbs			(GUI_GRID_WAbs / 1.2)
-#define GUI_GRID_W			(GUI_GRID_WAbs / 40)
-#define GUI_GRID_H			(GUI_GRID_HAbs / 25)
-#define GUI_GRID_X			(safezoneX)
-#define GUI_GRID_Y			(safezoneY + safezoneH - GUI_GRID_HAbs)
+#define GUI_GRID_WAbs       ((safezoneW / safezoneH) min 1.2)
+#define GUI_GRID_HAbs       (GUI_GRID_WAbs / 1.2)
+#define GUI_GRID_W          (GUI_GRID_WAbs / 40)
+#define GUI_GRID_H          (GUI_GRID_HAbs / 25)
+#define GUI_GRID_X          (safezoneX)
+#define GUI_GRID_Y          (safezoneY + safezoneH - GUI_GRID_HAbs)
 
 // Default text sizes
-#define GUI_TEXT_SIZE_SMALL		(GUI_GRID_H * 0.8)
-#define GUI_TEXT_SIZE_MEDIUM		(GUI_GRID_H * 1)
-#define GUI_TEXT_SIZE_LARGE		(GUI_GRID_H * 1.2)
+#define GUI_TEXT_SIZE_SMALL     (GUI_GRID_H * 0.8)
+#define GUI_TEXT_SIZE_MEDIUM    (GUI_GRID_H * 1)
+#define GUI_TEXT_SIZE_LARGE     (GUI_GRID_H * 1.2)
 
 // Pixel grid
-#define pixelScale	0.50
+#define pixelScale    0.50
 #define GRID_W (pixelW * pixelGrid * pixelScale)
 #define GRID_H (pixelH * pixelGrid * pixelScale)
 
