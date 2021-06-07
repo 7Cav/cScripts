e3cc52d61997fdeb290b1f3366f465f12657bd8b
Andreas Brostrom <andreas.brostrom.ce@gmail.com>
2021-06-07 22:28:46 +0200

    Removed unused files

--- cScripts/CfgLoadouts.hpp
+++ cScripts/CfgLoadouts.hpp
@@ -5,13 +5,11 @@ class CfgLoadouts {
     #include "Loadouts\CfgLoadouts_Alpha_Rotary.hpp"
     #include "Loadouts\CfgLoadouts_Alpha_FixedWing.hpp"
 
-    #include "Loadouts\CfgLoadouts_Bravo.hpp"
     #include "Loadouts\CfgLoadouts_Bravo_Leadership.hpp"
     #include "Loadouts\CfgLoadouts_Bravo_Crew.hpp"
     #include "Loadouts\CfgLoadouts_Bravo_Atlas.hpp"
     #include "Loadouts\CfgLoadouts_Bravo_Viking.hpp"
 
-    #include "Loadouts\CfgLoadouts_Charlie.hpp"
     #include "Loadouts\CfgLoadouts_Charlie_Squad.hpp"
     #include "Loadouts\CfgLoadouts_Charlie_Weapon.hpp"
 
