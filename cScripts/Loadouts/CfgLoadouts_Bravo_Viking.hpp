951e377076aa28724bdf989e073396cf317e1b43
Geki.T <46270872+Robot-Panda15@users.noreply.github.com>
2021-07-25 17:00:01 -0500

    Add Engineer Perms to Stryker Crews

--- cScripts/Loadouts/CfgLoadouts_Bravo_Viking.hpp
+++ cScripts/Loadouts/CfgLoadouts_Bravo_Viking.hpp
@@ -13,6 +13,7 @@ class Cav_B_B_Scout_PlatoonLeader_F : Cav_B_B_Scout_Officer_F {
 class Cav_B_B_Scout_PlatoonSergeant_F : Cav_B_B_Scout_Officer_F {
     displayName = "Viking Platoon Sergeant";
     loadout = [["rhs_weap_m4a1_mstock","","rhsusf_acc_anpeq15","rhsusf_acc_ACOG_RMR",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_kac_grip"],[],[],["rhs_uniform_acu_ocp",[["ACE_quikclot",16],["ACE_EarPlugs",1],["ACE_splint",2],["ACE_tourniquet",4],["ACE_MapTools",1],["ACE_microDAGR",1],["ACE_Flashlight_XL50",1],["ACRE_PRC343",1]]],["rhsusf_spcs_ocp_squadleader",[["ACRE_PRC152",1],["ACE_Chemlight_IR",2,1],["SmokeShellGreen",1,1],["HandGrenade",2,1],["SmokeShell",4,1],["rhs_mag_30Rnd_556x45_M855A1_PMAG",5,30],["rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2,30],[["ACE_MX2A","","","",[],[],""],1]]],["rhsusf_assault_eagleaiii_ocp",[["ACE_EntrenchingTool",1],["ACE_HuntIR_monitor",1],["ACRE_PRC152",1],["B_IR_Grenade",4,1],["SmokeShellBlue",4,1],["SmokeShellRed",4,1],["SmokeShellYellow",4,1]]],"rhsusf_ach_helmet_headset_ocp_alt","rhsusf_shemagh2_gogg_od",["ACE_Vector","","","",[],[],""],["ItemMap","ItemcTab","","ItemCompass","ItemWatch","rhsusf_ANPVS_14"]];
+    abilityEngineer = 1;
 };
 class Cav_B_B_Scout_PlatoonMedic_F : Cav_B_Bravo_base_F {
     displayName = "Viking Platoon Medic";
@@ -110,16 +111,19 @@ class Cav_B_B_Crew_F : Cav_B_Bravo_base_F {
     scope = 2;
     category[] += {"cScripts_Loadout_Cat_Bravo_Viking_Squad"};
     loadout = [["rhs_weap_m4a1_mstock","","rhsusf_acc_anpeq15","rhsusf_acc_compm4",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_kac_grip"],[],[],["rhs_uniform_acu_ocp",[["ACRE_PRC343",1],["ACE_quikclot",16],["ACE_EarPlugs",1],["ACE_splint",2],["ACE_tourniquet",4],["ACE_MapTools",1],["ACE_microDAGR",1],["ACE_Flashlight_XL50",1]]],["rhsusf_spcs_ocp_rifleman",[["ACRE_PRC152",1],["ACE_Chemlight_IR",2,1],["SmokeShellGreen",1,1],["HandGrenade",2,1],["SmokeShell",4,1],["rhs_mag_30Rnd_556x45_M855A1_PMAG",7,30]]],["rhsusf_assault_eagleaiii_ocp",[["ACE_EntrenchingTool",1],["ToolKit",1]]],"rhsusf_cvc_green_helmet","rhsusf_shemagh2_gogg_od",["ACE_Vector","","","",[],[],""],["ItemMap","ItemAndroid","","ItemCompass","ItemWatch","rhsusf_ANPVS_14"]];
+    abilityEngineer = 1;
 };
 class Cav_B_B_Ifv_Driver_F : Cav_B_B_Crew_F {
     displayName = "Stryker Driver";
     scope = 2;
     loadout = [["rhs_weap_m4a1_mstock","","rhsusf_acc_anpeq15","rhsusf_acc_compm4",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_kac_grip"],[],[],["rhs_uniform_acu_ocp",[["ACRE_PRC343",1],["ACE_quikclot",16],["ACE_EarPlugs",1],["ACE_splint",2],["ACE_tourniquet",4],["ACE_MapTools",1],["ACE_microDAGR",1],["ACE_Flashlight_XL50",1]]],["rhsusf_spcs_ocp_rifleman",[["ACRE_PRC152",1],["ACE_Chemlight_IR",2,1],["SmokeShellGreen",1,1],["HandGrenade",2,1],["SmokeShell",4,1],["rhs_mag_30Rnd_556x45_M855A1_PMAG",7,30]]],["rhsusf_assault_eagleaiii_ocp",[["ACE_EntrenchingTool",1],["ToolKit",1]]],"rhsusf_cvc_green_helmet","rhsusf_shemagh2_gogg_od",["ACE_Vector","","","",[],[],""],["ItemMap","ItemAndroid","","ItemCompass","ItemWatch","rhsusf_ANPVS_14"]];
+    abilityEngineer = 1;
 };
 class Cav_B_B_Ifv_Commander_F : Cav_B_B_Crew_F {
     displayName = "Stryker Vehicle Commander";
     scope = 2;
     loadout = [["rhs_weap_m4a1_mstock","","rhsusf_acc_anpeq15","rhsusf_acc_ACOG_RMR",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_kac_grip"],[],[],["rhs_uniform_acu_ocp",[["ACE_quikclot",16],["ACE_EarPlugs",1],["ACE_splint",2],["ACE_tourniquet",4],["ACE_MapTools",1],["ACE_microDAGR",1],["ACE_Flashlight_XL50",1],["ACRE_PRC343",1]]],["rhsusf_spcs_ocp_teamleader",[["ACRE_PRC152",1],["ACE_Chemlight_IR",2,1],["SmokeShellGreen",1,1],["HandGrenade",2,1],["SmokeShell",4,1],["rhs_mag_30Rnd_556x45_M855A1_PMAG",5,30],["rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2,30],[["ACE_MX2A","","","",[],[],""],1]]],["rhsusf_assault_eagleaiii_ocp",[["ACE_EntrenchingTool",1],["ACE_HuntIR_monitor",1],["B_IR_Grenade",4,1],["SmokeShellBlue",2,1],["SmokeShellRed",2,1],["SmokeShellYellow",2,1]]],"rhsusf_cvc_green_alt_helmet","rhsusf_shemagh2_gogg_od",["ACE_Vector","","","",[],[],""],["ItemMap","ItemAndroid","","ItemCompass","ItemWatch","rhsusf_ANPVS_14"]];
+    abilityEngineer = 1;
 };
 
 // Named
