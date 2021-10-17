f13c4406b7c3dfea8199e79ac2cbea3c509908ff
Geki.T <46270872+Robot-Panda15@users.noreply.github.com>
2021-10-17 11:36:02 -0500

    Update Charlie CLS and Platoon Medic with ACE Phar

--- cScripts/Loadouts/CfgLoadouts_Charlie_Squad.hpp
+++ cScripts/Loadouts/CfgLoadouts_Charlie_Squad.hpp
@@ -23,9 +23,10 @@ class Cav_B_C_PlatoonMedic_F : Cav_B_Charlie_base_F {
     displayName = "$STR_Cav_Charlie_Characters_C_PlatoonMedic";
     category[] += {"cScripts_Loadout_Cat_Charlie_Leadership"};
     scope = 2;
-    loadout = [["rhs_weap_m4a1","rhsusf_acc_SFMB556","rhsusf_acc_anpeq15_bk","rhsusf_acc_compm4",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_grip3_tan"],[],[],["rhs_uniform_acu_oefcp",[["ACRE_PRC343",1],["ACE_MapTools",1],["ACE_Flashlight_XL50",1],["ACRE_PRC152",1],["ACE_microDAGR",1],["ACE_splint",2],["ACE_CableTie",2]]],["rhsusf_spcs_ocp_medic",[["ACE_surgicalKit",1],["rhs_mag_30Rnd_556x45_M855A1_PMAG",7,30],["rhs_mag_m67",2,1],["SmokeShell",4,1],["ACE_Chemlight_HiRed",4,1],["ACE_Chemlight_HiYellow",4,1]]],["B_Kitbag_mcamo",[["ACE_quikclot",24],["ACE_tourniquet",10],["rhsusf_ANPVS_14",1],["ACE_personalAidKit",1],["ACE_elasticBandage",30],["ACE_morphine",20],["ACE_epinephrine",20],["ACE_adenosine",6],["ACE_salineIV",12],["ACE_splint",10]]],"rhsusf_ach_helmet_camo_ocp","rhs_googles_black",["rhsusf_bino_m24_ARD","","","",[],[],""],["ItemMap","","ItemRadioAcreFlagged","ItemCompass","ACE_Altimeter",""]];
+    loadout = [["rhs_weap_m4a1","rhsusf_acc_SFMB556","rhsusf_acc_anpeq15_bk","rhsusf_acc_compm4",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_grip3_tan"],[],[],["rhs_uniform_acu_oefcp",[["ACRE_PRC343",1],["ACE_MapTools",1],["ACE_Flashlight_XL50",1],["ACRE_PRC152",1],["ACE_microDAGR",1],["ACE_splint",2],["ACE_CableTie",2],["ACE_quikclot",12]]],["rhsusf_spcs_ocp_medic",[["ACE_surgicalKit",1],["kat_carbonate",2],["rhs_mag_30Rnd_556x45_M855A1_PMAG",6,30],["rhs_mag_m67",2,1],["SmokeShell",4,1],["ACE_Chemlight_HiYellow",4,1],["kat_Painkiller",3,10]]],["B_Kitbag_mcamo",[["ACE_tourniquet",10],["rhsusf_ANPVS_14",1],["ACE_elasticBandage",30],["ACE_morphine",5],["ACE_epinephrine",14],["ACE_adenosine",6],["ACE_salineIV",10],["ACE_splint",10],["kat_naloxone",4],["ACE_salineIV_500",8],["kat_TXA",4],["kat_IO_FAST",4],["kat_IV_16",10],["ACE_packingBandage",15]]],"rhsusf_ach_helmet_camo_ocp","rhs_googles_black",["rhsusf_bino_m24_ARD","","","",[],[],""],["ItemMap","","ItemRadioAcreFlagged","ItemCompass","ACE_Altimeter",""]];
     insignia = "cav_insignia_specialized_cls";
     abilityMedic = 1;
+    role = "medic";
     icon = "iconManMedic";
 };
 
@@ -79,8 +80,8 @@ class Cav_B_C_CombatLifeSaver_F : Cav_B_Charlie_base_F {
     displayName = "$STR_Cav_Charlie_Characters_C_CombatLifeSaver";
     scope = 2;
     category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    loadout = [["rhs_weap_m4a1","rhsusf_acc_SFMB556","rhsusf_acc_anpeq15_bk","rhsusf_acc_compm4",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_grip1"],[],[],["rhs_uniform_acu_oefcp",[["ACRE_PRC343",1],["ACE_MapTools",1],["ACE_Flashlight_XL50",1],["ACE_quikclot",14],["ACE_tourniquet",4],["ACE_DAGR",1]]],["rhsusf_spcs_ocp_medic",[["rhs_mag_30Rnd_556x45_M855A1_PMAG",9,30],["rhs_mag_m67",3,1],["SmokeShell",4,1],["ACE_Chemlight_HiRed",4,1]]],["rhsusf_assault_eagleaiii_ocp",[["rhsusf_ANPVS_14",1],["ACE_quikclot",20],["ACE_tourniquet",12],["ACE_splint",8],["ACE_EntrenchingTool",1],["kat_IV_16",6],["kat_carbonate",2],["ACE_epinephrine",8],["kat_naloxone",4],["ACE_packingBandage",30],["kat_Painkiller",3,10]]],"rhsusf_ach_helmet_camo_ocp","rhs_googles_black",[],["ItemMap","","ItemRadioAcreFlagged","ItemCompass","ACE_Altimeter",""]];
     insignia = "cav_insignia_specialized_cls";
-    loadout = [["rhs_weap_m4a1","rhsusf_acc_SFMB556","rhsusf_acc_anpeq15_bk","rhsusf_acc_compm4",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_tdstubby_tan"],[],[],["rhs_uniform_acu_oefcp",[["ACRE_PRC343",1],["ACE_MapTools",1],["ACE_Flashlight_XL50",1],["ACE_quikclot",14],["ACE_tourniquet",4],["ACE_DAGR",1]]],["rhsusf_spcs_ocp_medic",[["rhs_mag_30Rnd_556x45_M855A1_PMAG",9,30],["rhs_mag_m67",3,1],["SmokeShell",4,1],["ACE_Chemlight_HiRed",4,1]]],["B_AssaultPack_mcamo",[["rhsusf_ANPVS_14",1],["ACE_quikclot",20],["ACE_tourniquet",4],["ACE_personalAidKit",5],["ACE_splint",8],["ACE_EntrenchingTool",1],["rhs_mag_m67",1,1]]],"rhsusf_ach_helmet_camo_ocp","rhs_googles_black",[],["ItemMap","","ItemRadioAcreFlagged","ItemCompass","ACE_Altimeter",""]];
     abilityMedic = 0;
     icon = "iconManMedic";
 };
