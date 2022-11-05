1d556496705211b88585b685b62f4b4bc1ee321f
Geki.T <46270872+Robot-Panda15@users.noreply.github.com>
2022-11-05 01:54:34 -0500

    Add Bravo WSL

--- cScripts/Loadouts/CfgLoadouts_Bravo_Viking.hpp
+++ cScripts/Loadouts/CfgLoadouts_Bravo_Viking.hpp
@@ -46,6 +46,15 @@ class Cav_B_B_Scout_SquadLeader_F : Cav_B_B_Scout_Base_F {
     role = "squadleader";
     icon = "iconManLeader";
 };
+
+class Cav_B_B_Scout_WSL_F_Local : Cav_B_Bravo_base_F {
+    displayName = "Weapons Squad Leader";
+    scope = 2;
+    category[] += {"cScripts_Loadout_Cat_Bravo_Viking_Leadership"};
+    loadout = [["rhs_weap_m4a1_blockII","","rhsusf_acc_anpeq15","rhsusf_acc_ACOG_RMR",["rhs_mag_30Rnd_556x45_M855A1_PMAG",30],[],"rhsusf_acc_grip2"],[],["rhs_weap_M320","","","",["rhs_mag_m713_Red",1],[],""],["USP_G3C_KP_OR_MC",[["ACE_quikclot",16],["ACE_EarPlugs",1],["ACE_splint",2],["ACE_tourniquet",4],["ACE_MapTools",1],["ACE_microDAGR",1],["ACE_Flashlight_XL50",1],["ACE_artilleryTable",1],["ACRE_PRC343",1]]],["USP_CRYE_CPC_WEAPON_BELT_MC",[["ACRE_PRC152",1],["ACE_Chemlight_IR",2,1],["SmokeShellGreen",1,1],["HandGrenade",2,1],["SmokeShell",4,1],["rhs_mag_30Rnd_556x45_M855A1_PMAG",5,30],["rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2,30],["rhs_mag_m713_Red",2,1],[["ACE_MX2A","","","",[],[],""],1]]],["UK3CB_BAF_B_Bergen_MTP_SL_L_A",[["ACE_EntrenchingTool",1],["ACE_HuntIR_monitor",1],["B_IR_Grenade",4,1],["SmokeShellBlue",4,1],["SmokeShellRed",4,1],["SmokeShellYellow",4,1],["ACE_Chemlight_HiYellow",2,1],["ACE_Chemlight_HiRed",2,1],["ACE_Chemlight_HiBlue",2,1],["rhs_mag_m714_White",5,1],["rhs_mag_M664_red_cluster",4,1]]],"USP_OPS_FASTXP_TAN_MC_08","USP_MFRAME_SMG1_BLK2",["ACE_Vector","","","",[],[],""],["ItemMap","ItemcTab","","ItemCompass","ItemWatch","USP_PVS31"]];
+    icon = "iconManLeader"
+};
+
 class Cav_B_B_Scout_TeamLeader_F : Cav_B_B_Scout_Base_F {
     displayName = "Scout Team Leader";
     scope = 2;
