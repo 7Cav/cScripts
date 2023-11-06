441208d1f9939934c00b1eebea9179bcebc37ac9
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-11-06 18:04:28 +0100

    Activated test for title cehck

--- tools/checkPullRequestTitle.sh
+++ tools/checkPullRequestTitle.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 _throw() {
-    echo "Pull Request: Bad name"
+    echo "FAILED: Pull Request has $1"
     exit 1
 }
-string="$*"
-if [[ ${#string} -lt 20 ]]; then _throw; fi
-    
 
+string="$*"
+
+if [[ ${#string} -lt 20 ]]; then _throw "short name"; fi
+if [[ $string == "Update cba_settings.sqf" ]]; then _throw "bad name"; fi
+    
+echo "SUCCESS"
 exit 0
