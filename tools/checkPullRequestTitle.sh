a9130e082b3b5eb389c4d6692e542cb0c6dee25a
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2023-11-07 09:09:04 +0100

    Fixed spelling in CI test (#1067)
    * Update CI test checkPullRequestTitle.sh
    
    * Update checkPullRequestTitle.sh

--- tools/checkPullRequestTitle.sh
+++ tools/checkPullRequestTitle.sh
@@ -10,11 +10,11 @@ string="$*"
 
 
 echo "Validating '$string'..."
-if [[ ${#string} == 0 ]];                       then echo "SUCCESS (Empty name)"; exit 0; fi
+if [[ ${#string} == 0 ]];                       then echo "SUCCESS: Empty name (Assumed bransh run)"; exit 0; fi
 
 if [[ ${#string} -lt 20 ]];                     then _throw "Short name"; fi
-if [[ $string == "Update cba_settings.sqf" ]];  then _throw "Sad name"; fi
-if [[ $string == "Update settings" ]];          then _throw "Sad name"; fi
+if [[ $string == "Update cba_settings.sqf" ]];  then _throw "Bad name"; fi
+if [[ $string == "Update settings" ]];          then _throw "Bad name"; fi
 
 # Descriptor
 if [[ $string != *"Added"* ]]; then
@@ -24,7 +24,7 @@ if [[ $string != *"Added"* ]]; then
                 if [[ $string != *"Updated"* ]]; then
                     if [[ $string != *"updated"* ]]; then
                         if [[ $string != *"Adjusted"* ]]; then
-                            if [[ $string != *"adjusted"* ]]; then _throw "missing descriptor [Added, Fixed, Adjusted, Updated]"; fi
+                            if [[ $string != *"adjusted"* ]]; then _throw "Missing descriptor [Added, Fixed, Adjusted, Updated]"; fi
                         fi
                     fi
                 fi
