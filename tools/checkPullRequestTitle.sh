818d27340ed0d4744351d5de3189487ade4a69e9
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2023-11-07 08:58:33 +0100

    Update CI test checkPullRequestTitle.sh

--- tools/checkPullRequestTitle.sh
+++ tools/checkPullRequestTitle.sh
@@ -12,9 +12,9 @@ string="$*"
 echo "Validating '$string'..."
 if [[ ${#string} == 0 ]];                       then echo "SUCCESS (Empty name)"; exit 0; fi
 
-if [[ ${#string} -lt 20 ]];                     then _throw "Short name"; fi
-if [[ $string == "Update cba_settings.sqf" ]];  then _throw "Sad name"; fi
-if [[ $string == "Update settings" ]];          then _throw "Sad name"; fi
+if [[ ${#string} -lt 20 ]];                     then _throw "short name"; fi
+if [[ $string == "Update cba_settings.sqf" ]];  then _throw "bad name"; fi
+if [[ $string == "Update settings" ]];          then _throw "bad name"; fi
 
 # Descriptor
 if [[ $string != *"Added"* ]]; then
