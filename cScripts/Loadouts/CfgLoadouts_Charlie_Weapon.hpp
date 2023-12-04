1ef6a5e6af4f306f0d4b62b71d7c45f988d5254d
Zarenx <111392464+Zarenx@users.noreply.github.com>
2023-12-04 16:44:30 +0100

    Charlie Weapons Squad MG Assistant Fix
    Fixed the error where the role missied both access arsenal and loadout when spawned in as the M240B Assistant in Misfit and Bandit Weapons Squads.

--- cScripts/Loadouts/CfgLoadouts_Charlie_Weapon.hpp
+++ cScripts/Loadouts/CfgLoadouts_Charlie_Weapon.hpp
@@ -21,7 +21,7 @@ class Cav_B_C_Weapons_M240B_Machinegunner_F: Cav_B_Charlie_Weapons_base_F {
     icon = "iconManMG";
 };
 
-class Cav_B_C_Weapons_M240B_AssistantMachinegunner_F: Cav_B_Charlie_Weapons_base_F {
+class Cav_B_C_Weapons_M240B_MachinegunnerAmmoBearer_F: Cav_B_Charlie_Weapons_base_F {
     displayName = "Machine Gunner Assistant";
     scope = 2;
     loadout = [["rhs_weap_m4a1_blockII_KAC","rhsusf_acc_SF3P556","rhsusf_acc_anpeq15","rhsusf_acc_ACOG_RMR",["rhs_mag_30Rnd_556x45_M855A1_PMAG_Tan",30],[],""],["dzn_MG_Tripod_M122A1_M240Mount_Carry","","","",[],[],""],[],["USP_G3C_RS2_KP_OR_MC",[["ACE_quikclot",10],["ACE_tourniquet",4],["ACE_Flashlight_XL50",1],["ACE_MapTools",1],["ACE_microDAGR",1],["ACE_splint",2],["kat_Painkiller",1,10]]],["USP_CRYE_CPC_FAST_BELT_MC",[["ACE_EntrenchingTool",1],["SmokeShell",3,1],["ACE_Chemlight_IR",2,1],["rhs_mag_30Rnd_556x45_M855A1_PMAG_Tan",9,30],["HandGrenade",2,1]]],["B_Kitbag_mcamo",[["USP_PVS15",1],["rhsusf_100Rnd_762x51_m62_tracer",6,100],["ACE_SpareBarrel",1,1]]],"USP_OPSCORE_FASTMTC_CW","",["ACE_Vector","","","",[],[],""],["ItemMap","ItemAndroid","","ItemCompass","ACE_Altimeter",""]];
@@ -69,10 +69,10 @@ class Cav_B_C_Weapons_SquadLeader_2_4_F: Cav_B_C_Weapons_SquadLeader_F { scope =
 
 class Cav_B_C_Weapons_Alpha_FireTeamLeader_F: Cav_B_C_Weapons_M240B_FireTeamLeader_F { scope = 1; };
 class Cav_B_C_Weapons_Alpha_Trooper_2_F: Cav_B_C_Weapons_M240B_Machinegunner_F { scope = 1; };
-class Cav_B_C_Weapons_Alpha_Trooper_3_F: Cav_B_C_Weapons_M240B_AssistantMachinegunner_F { scope = 1; };
+class Cav_B_C_Weapons_Alpha_Trooper_3_F: Cav_B_C_Weapons_M240B_MachinegunnerAmmoBearer_F { scope = 1; };
 class Cav_B_C_Weapons_Alpha_Trooper_4_F: Cav_B_C_Weapons_MAAWS_MAAWSGunner_F { scope = 1; };
 
 class Cav_B_C_Weapons_Bravo_FireTeamLeader_F: Cav_B_C_Weapons_M240B_FireTeamLeader_F { scope = 1; };
 class Cav_B_C_Weapons_Bravo_Trooper_2_F: Cav_B_C_Weapons_M240B_Machinegunner_F { scope = 1; };
-class Cav_B_C_Weapons_Bravo_Trooper_3_F: Cav_B_C_Weapons_M240B_AssistantMachinegunner_F { scope = 1; };
+class Cav_B_C_Weapons_Bravo_Trooper_3_F: Cav_B_C_Weapons_M240B_MachinegunnerAmmoBearer_F { scope = 1; };
 class Cav_B_C_Weapons_Bravo_Trooper_4_F: Cav_B_C_Weapons_MAAWS_MAAWSGunner_F { scope = 1; };
