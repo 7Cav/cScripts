d4a14084767f17fde5da510f2fd30ae254bc813d
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-05-20 14:26:58 +0200

    Updated bravo removed labeled for now

--- cScripts/Loadouts/CfgLoadouts_Bravo.hpp
+++ cScripts/Loadouts/CfgLoadouts_Bravo.hpp
@@ -1,143 +1,151 @@
-class Cav_B_B_Officer_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_Officer_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Leadership"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Leadership"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
 };
 class Cav_B_B_PlatoonSergeant_F: Cav_B_B_Officer_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Leadership"};
+    //category[] += {"cScripts_Loadout_Cat_Bravo_Leadership"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
 class Cav_B_B_PlatoonLeader_F: Cav_B_B_Officer_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Leadership"};
+    //category[] += {"cScripts_Loadout_Cat_Bravo_Leadership"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
 
 
-class Cav_B_B_SquadLeader_F: Cav_B_Charlie_base_F {
+class Cav_B_B_SquadLeader_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Leadership"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Leadership"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_FireTeamLeader_F: Cav_B_Charlie_base_F {
+class Cav_B_B_FireTeamLeader_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
 
-class Cav_B_B_Rifleman_F: Cav_B_Charlie_base_F {
+class Cav_B_B_Rifleman_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_AutomaticRifleman_F: Cav_B_Charlie_base_F {
+class Cav_B_B_AutomaticRifleman_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_AssistantAutomaticRifleman_F: Cav_B_Charlie_base_F { // Not used (scope 0)
-    scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
-    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
-    equipmentTags[] = {};
-};
-class Cav_B_B_Grenadier_F: Cav_B_Charlie_base_F {
+class Cav_B_B_AssistantAutomaticRifleman_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_RiflemanLAT_F: Cav_B_Charlie_base_F {
+class Cav_B_B_MachineGunner_F: Cav_B_Bravo_base_F { // Not used (scope 0)
+    scope = 0;
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
+    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
+    equipmentTags[] = {};
+};
+class Cav_B_B_AssistantMachineGunner_F: Cav_B_Bravo_base_F { // Not used (scope 0)
+    scope = 0;
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
+    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
+    equipmentTags[] = {};
+};
+class Cav_B_B_Grenadier_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-
-class Cav_B_B_MachineGunner_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_RiflemanLAT_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_AssistantMachineGunner_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_RiflemanAT_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_RiflemanAT_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_RiflemanATAssistant_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_RiflemanATAssistant_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_RiflemanAA_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_RiflemanAA_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_RiflemanAAAssistant_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_RiflemanAAAssistant_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_RiflemanATR_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_RiflemanATR_F: Cav_B_Charlie_base_F { // Not used (scope 0)
+class Cav_B_B_RiflemanATRAssistant_F: Cav_B_Bravo_base_F { // Not used (scope 0)
     scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Weapons"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
 };
-class Cav_B_B_RiflemanATRAssistant_F: Cav_B_Charlie_base_F { // Not used (scope 0)
-    scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Weapons"};
-    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
-    equipmentTags[] = {};
-};
-
-class Cav_B_B_Marksman_F: Cav_B_Charlie_base_F {
+class Cav_B_B_PlatoonMedic_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Special"};
-    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
-    equipmentTags[] = {};
-};
-
-
-class Cav_B_B_Medic_F: Cav_B_Charlie_base_F { // Not used (scope 0)
-    scope = 0;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
-    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
-    equipmentTags[] = {};
-    abilityMedic = 3;
-};
-class Cav_B_B_PlatoonMedic_F: Cav_B_Charlie_base_F {
-    scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Leadership"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
     abilityMedic = 2;
 };
-class Cav_B_B_CombatLifeSaver_F: Cav_B_Charlie_base_F {
+class Cav_B_B_Medic_F: Cav_B_Bravo_base_F { // Not used (scope 0)
+    scope = 0;
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
+    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
+    equipmentTags[] = {};
+    abilityMedic = 3;
+};
+class Cav_B_B_CombatLifeSaver_F: Cav_B_Bravo_base_F {
     scope = 2;
-    category[] += {"cScripts_Loadout_Cat_Charlie_Squad"};
+    category[] += {"cScripts_Loadout_Cat_Bravo_Squad"};
     loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
     equipmentTags[] = {};
     abilityMedic = 1;
 };
 
-//FIXME: Add first platoon units. Here basically
+
+class Cav_B_B_IFV_Commander_F: Cav_B_Bravo_base_F {
+    scope = 2;
+    category[] += {"cScripts_Loadout_Cat_Bravo_IFV"};
+    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
+    equipmentTags[] = {};
+};
+class Cav_B_B_IFV_Driver_F: Cav_B_Bravo_base_F {
+    scope = 2;
+    category[] += {"cScripts_Loadout_Cat_Bravo_IFV"};
+    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
+    equipmentTags[] = {};
+};
+class Cav_B_B_IFV_Gunner_F: Cav_B_Bravo_base_F {
+    scope = 2;
+    category[] += {"cScripts_Loadout_Cat_Bravo_IFV"};
+    loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
+    equipmentTags[] = {};
+};
