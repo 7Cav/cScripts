6a3e6588d2076657c1f33edbf1b796a926f4f61c
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-05-20 14:54:39 +0200

    Added bravo

---
+++ cScripts/Loadouts/CfgLoadouts_Bravo_Labeled.hpp
@@ -0,0 +1,230 @@
+// Second Platoon
+class Cav_B_B_PlatoonSergeant_2_5_F: Cav_B_B_PlatoonSergeant_F: 2: 5);
+class Cav_B_B_PlatoonLeader_2_6_F: Cav_B_B_PlatoonLeader_F: 2: 6);
+class Cav_B_B_PlatoonMedic_2_7_F: Cav_B_B_PlatoonMedic_F: 2: 7);
+
+// Squad 1-2-B-1/7
+class Cav_B_B_SquadLeader_2_1_F: Cav_B_B_SquadLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    insignia = "cav_insignia_charlie_2_1";
+};
+
+class Cav_B_B_FireTeamLeader_2_1_A_F: Cav_B_B_FireTeamLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_1";
+};
+class Cav_B_B_AutomaticRifleman_2_1_A_F: Cav_B_B_AutomaticRifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_1";
+};
+class Cav_B_B_Grenadier_2_1_A_F: Cav_B_B_Grenadier_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_1";
+};
+class Cav_B_B_Rifleman_2_1_A_F: Cav_B_B_Rifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_1";
+};
+
+class Cav_B_B_FireTeamLeader_2_1_B_F: Cav_B_B_FireTeamLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_1";
+};
+class Cav_B_B_AutomaticRifleman_2_1_B_F: Cav_B_B_AutomaticRifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_1";
+};
+class Cav_B_B_Grenadier_2_1_B_F: Cav_B_B_Grenadier_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_1";
+};
+class Cav_B_B_CombatLifeSaver_2_1_B_F: Cav_B_B_CombatLifeSaver_F {
+    scope = 1;
+    platoon = 2;
+    squad = 1;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_1";
+};
+
+// Squad 2-1-B-1/7
+class Cav_B_B_SquadLeader_2_2_F: Cav_B_B_SquadLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    insignia = "cav_insignia_charlie_2_2";
+};
+
+class Cav_B_B_FireTeamLeader_2_2_A_F: Cav_B_B_FireTeamLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_2";
+};
+class Cav_B_B_AutomaticRifleman_2_2_A_F: Cav_B_B_AutomaticRifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_2";
+};
+class Cav_B_B_Grenadier_2_2_A_F: Cav_B_B_Grenadier_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_2";
+};
+class Cav_B_B_Rifleman_2_2_A_F: Cav_B_B_Rifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_2";
+};
+
+class Cav_B_B_FireTeamLeader_2_2_B_F: Cav_B_B_FireTeamLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_2";
+};
+class Cav_B_B_AutomaticRifleman_2_2_B_F: Cav_B_B_AutomaticRifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_2";
+};
+class Cav_B_B_Grenadier_2_2_B_F: Cav_B_B_Grenadier_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_2";
+};
+class Cav_B_B_CombatLifeSaver_2_2_B_F: Cav_B_B_CombatLifeSaver_F {
+    scope = 1;
+    platoon = 2;
+    squad = 2;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_2";
+};
+
+// Squad 3-1-B-1/7
+class Cav_B_B_SquadLeader_2_3_F: Cav_B_B_SquadLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    insignia = "cav_insignia_charlie_2_3";
+};
+
+class Cav_B_B_FireTeamLeader_2_3_A_F: Cav_B_B_FireTeamLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_3";
+};
+class Cav_B_B_AutomaticRifleman_2_3_A_F: Cav_B_B_AutomaticRifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_3";
+};
+class Cav_B_B_Grenadier_2_3_A_F: Cav_B_B_Grenadier_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_3";
+};
+class Cav_B_B_Rifleman_2_3_A_F: Cav_B_B_Rifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "alpha";
+    insignia = "cav_insignia_charlie_2_3";
+};
+
+class Cav_B_B_FireTeamLeader_2_3_B_F: Cav_B_B_FireTeamLeader_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_3";
+};
+class Cav_B_B_AutomaticRifleman_2_3_B_F: Cav_B_B_AutomaticRifleman_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_3";
+};
+class Cav_B_B_Grenadier_2_3_B_F: Cav_B_B_Grenadier_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_3";
+};
+class Cav_B_B_CombatLifeSaver_2_3_B_F: Cav_B_B_CombatLifeSaver_F {
+    scope = 1;
+    platoon = 2;
+    squad = 3;
+    team = "bravo";
+    insignia = "cav_insignia_charlie_2_3";
+};
+
+// Squad 4-1-B-1/7
+//FIXME: class Cav_B_B_SquadLeader_2_4_F: Cav_B_B_SquadLeader_F: {};
+
+//FIXME: class Cav_B_B_FireTeamLeader_2_4_A_F: Cav_B_B_FireTeamLeader_F {};
+//FIXME: class Cav_B_B_AutomaticRifleman_2_4_A_F: Cav_B_B_AutomaticRifleman_F {};
+//FIXME: class Cav_B_B_Grenadier_2_4_A_F: Cav_B_B_Grenadier_F {};
+//FIXME: class Cav_B_B_Rifleman_2_4_A_F: Cav_B_B_Rifleman_F {};
+
+//FIXME: class Cav_B_B_FireTeamLeader_2_4_B_F: Cav_B_B_FireTeamLeader_F {};
+//FIXME: class Cav_B_B_AutomaticRifleman_2_4_B_F: Cav_B_B_AutomaticRifleman_F {};
+//FIXME: class Cav_B_B_Grenadier_2_4_B_F: Cav_B_B_Grenadier_F {};
+//FIXME: class Cav_B_B_CombatLifeSaver_2_4_B_F: Cav_B_B_CombatLifeSaver_F {};
+
+// Vehicle Crew Units
+//FIXME: class Cav_B_B_IFV_Commander_2_5_F: Cav_B_B_IFV_Commander_F {};
+//FIXME: class Cav_B_B_IFV_Driver_2_5_F: Cav_B_B_IFV_Driver_F {};
+//FIXME: class Cav_B_B_IFV_Commander_2_6_F: Cav_B_B_IFV_Commander_F {};
+//FIXME: class Cav_B_B_IFV_Driver_2_6_F: Cav_B_B_IFV_Driver_F {};
+
+//FIXME: class Cav_B_B_IFV_Commander_2_1_F: Cav_B_B_IFV_Commander_F {};
+//FIXME: class Cav_B_B_IFV_Gunner_2_1_F: Cav_B_B_IFV_Gunner_F {};
+//FIXME: class Cav_B_B_IFV_Commander_2_2_F: Cav_B_B_IFV_Commander_F {};
+//FIXME: class Cav_B_B_IFV_Gunner_2_2_F: Cav_B_B_IFV_Gunner_F {};
+//FIXME: class Cav_B_B_IFV_Commander_2_3_F: Cav_B_B_IFV_Commander_F {};
+//FIXME: class Cav_B_B_IFV_Gunner_2_3_F: Cav_B_B_IFV_Gunner_F {};
+//FIXME: class Cav_B_B_IFV_Commander_2_4_F: Cav_B_B_IFV_Commander_F {};
+//FIXME: class Cav_B_B_IFV_Gunner_2_4_F: Cav_B_B_IFV_Gunner_F {};
